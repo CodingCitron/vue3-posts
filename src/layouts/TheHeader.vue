74bd21849c63ca45b9afc560df9eac2cc2a70640
CodingCitron <xectler@gmail.com>
2023-10-06 07:02:09 +0900

    update

---
+++ src/layouts/TheHeader.vue
@@ -0,0 +1,45 @@
+<script setup></script>
+<template>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">SANGMIN CODING</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<router-link class="nav-link" aria-current="page" to="/"
+								>Home</router-link
+							>
+						</li>
+						<li class="nav-item">
+							<RouterLink class="nav-link" to="/about">About</RouterLink>
+						</li>
+					</ul>
+					<form class="d-flex" role="search">
+						<input
+							class="form-control me-2"
+							type="search"
+							placeholder="Search"
+							aria-label="Search"
+						/>
+						<button class="btn btn-outline-success" type="submit">
+							Search
+						</button>
+					</form>
+				</div>
+			</div>
+		</nav>
+	</header>
+</template>
+<style scoped></style>
